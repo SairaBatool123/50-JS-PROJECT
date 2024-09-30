43dd1c710f5b5598d4d737f6c38886166db1a21a
SairaBatool123 <sairabatool787@gmail.com>
2024-09-30 21:10:00 +0500

    Done

---
+++ QR-CODE-GENERATOR/app.js
@@ -0,0 +1,16 @@
+let qrinput = document.querySelector('#qr-input');
+let qrbtn = document.querySelector('#qr-btn');
+let qrimg = document.querySelector('#qr-img');
+
+// console.log(qrbtn , qrinput , qrimg);
+
+qrbtn.addEventListener('click', function () {
+    let input = qrinput.value
+    if (input == '') {           //(!input)
+        alert('hi')
+    } else {
+        //link of api https://goqr.me/api/
+        qrimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`;
+        qrimg.alt = `QR Code for ${input}`
+    }
+})
